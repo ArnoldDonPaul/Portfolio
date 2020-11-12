dd5b4c7c5ad6499ed09946cca56f9ac7c58016f9
Arnold <arnolddonpaul@gmail.com>
2020-11-12 17:45:44 -0500

    created typing effect. began styling. breakpoints are not working correctly

--- my-portfolio/src/components/hero.js
+++ my-portfolio/src/components/hero.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import '../styles/styles.css';
+import '../styles/components/hero/hero.scss';
 
 //array of text that will populate onClick
 let myArray = [
     //fun
     "because I couldn't become Spider-Man.",
-    "and all of your base are belong to me.",
-    "&lt;div&gt;{funnyText}&lt;/div&gt;",
+    "& all of your base are belong to me.",
+    "& <div>{funnyText}</div>",
     "who will never not pet a dog.",
-    "and a black belt in Kung Fu.",
+    "& black belt in Kung Fu.",
     "who thinks alpacas are delightful.",
-    "and I'm happy you're here. Welcome!",
-    "and coffee is my life force",
-    "by day, asleep by night",
-    "and sometimes a huge nerd",
+    "& happy you're here. Welcome!",
+    "& coffee is my life force.",
+    "by day, asleep by night.",
+    "& sometimes a huge nerd.",
+    "& amateur card magician.",
+    "& occasional rock climber.",
+    "of Slytherin House.",
     //less fun
-    "and I built this site using React.",
+    "that built this site using React.",
     "who is constantly learning.",
     "that's open to new opportunities.",
     "with an eye for detail.",
-    "with a UX design certificate",
-    "who collaborates well with others",
-    "that creates user-friendly experiences",
+    "with a UX design certificate.",
+    "who collaborates well with others.",
+    "that creates user-friendly experiences.",
 ];
 
 //set the length of the array to a variable to make it dynamic
@@ -36,20 +39,21 @@ class Hero extends Component {
         //function to change displayed text onClick
         const changeDisplay = () => {
 
+            document.getElementById("randomized").innerHTML = ""
+
             //sets an empty array to store value of displayed array item
             let arrayItem = []
 
             //select a random item from myArray
             let newItem = myArray[Math.floor(Math.random() * myArray.length)];
 
-            //sets the displayed text to the random item from myArray
-            document.getElementById("randomized").innerHTML = newItem
-
             //store the currently displayed array item in arrayItem for comparison
-            arrayItem.push(document.getElementById("randomized").innerHTML)
+            arrayItem.push(newItem)
 
             //store a copy of the original array
-            newArray.push(document.getElementById("randomized").innerHTML)
+            if (arrayItem !== "") {
+                newArray.push(String(arrayItem))
+            }
 
             //compare myArray to the currently displayed item. remove it from myArray
             var i
@@ -64,15 +68,36 @@ class Hero extends Component {
                     myArray.push(newArray[i])
                 }
                 newArray.splice(0, arrayLength)
-                newArray.push(document.getElementById("randomized").innerHTML)
+                newArray.push(String(arrayItem))
             }
+
+            var j = 0
+            function type() {
+                if (j < newItem.length) {
+                    document.getElementById("randomized").innerHTML += newItem.charAt(j);
+                    j++;
+                    setTimeout(type, 10)
+                }
+            }
+
+
+            type()
+
+            console.log(myArray)
+            console.log(newArray)
+            console.log(arrayItem)
+            console.log(newItem)
         }
 
         return (
             <>
-                <p>Arnold Don Paul</p>
-                <p>I'm a Web Developer <span id="randomized" >based in Toronto, Canada.</span></p>
-                <button onClick={changeDisplay}>click me!</button>
+                <div className="hero">
+                    <p className="hero__text">web developer </p>
+                    <div className="hero__dynamicSection">
+                        <p><span className="hero__text--dynamic" id="randomized">based in Toronto, Canada.</span><span className="hero__text--blink">|</span></p>
+                        <div className="hero__button" onClick={changeDisplay}><span className="hero__button--dice" role="img" aria-label="dice">🎲</span></div>
+                    </div>
+                </div>
             </>
         );
     }
--- my-portfolio/src/pages/Main.js
+++ my-portfolio/src/pages/Main.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+
+import '../styles/pages/Main/Main.scss';
+
 import Hero from '../components/hero';
-import '../styles/styles.css';
+
 
 function Main() {
   return (
     <BrowserRouter>
-      <Route component={Hero} />
+      <div className="navComponent">
+        <Route component={Hero} />
+      </div>
     </BrowserRouter>
   );
 }
